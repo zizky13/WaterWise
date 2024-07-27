2db6b3e8c17b0709ec19df5e35cdbb68f25622d9
dikaarnnd <dika4kiga@gmail.com>
2024-07-27 23:58:46 +0700

    add_todolist

--- server/config.js
+++
@@ -1,3 +0,0 @@
-export const PORT = 8080;
-export const dbUri =
-  "mongodb+srv://root:cS47y9iZfybTE4nf@cluster.3c4djxr.mongodb.net/water-wise?retryWrites=true&w=majority&appName=Cluster";
--- src/pages/mainMenus/dashboard.js
+++ src/pages/mainMenus/dashboard.js
@@ -1,21 +1,11 @@
 import React, { useState } from 'react';
 import profile from '../../assets/images/google.png';
+import ToDoList from '../../components/todolist.js';
 
 export default function Dasboard() {
     return (
         <div className='flex-1 overflow-hidden'>
-            <div className="flex flex-col bg-green-400">
-                <div className="flex flex-row">
-                    <img src={profile} className='w-20 h'/>
-                    <span>p</span>
-                </div>
-            </div>
-            <div>
-                <span>span</span>
-            </div>
-            <div>
-                <span>p</span>
-            </div>
+            <ToDoList/>
         </div>
     )
 }
